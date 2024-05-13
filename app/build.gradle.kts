f4050bc59d93848b1047f0bc6127c12a4723c0db
ngud-119 <passiondev7@gmail.com>
2024-05-13 22:57:22 +0700

    Update tos and privacy policy path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.harissabil.fisch"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
